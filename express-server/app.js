b0c94f7244fb4199b70a2073052837a16e3471e1
jun1216 <junchao.chen@mail.utoronto.ca>
2021-07-18 20:42:52 -0400

    segregated client from express server, created boiler plate react client

--- express-server/app.js
+++ express-server/app.js
@@ -1,21 +1,18 @@
 "use strict";
 const express = require('express');
-const mongoose = require('mongoose');
 const session = require('express-session');
 var passport = require('passport');
-var crypto = require('crypto');
 var routes = require('./routes');
 const connection = require('./config/database');
 
 const MongoStore = require('connect-mongo')(session);
 
-require('./config/passport');
+require('dotenv').config();
 
 /**
  * -------------- GENERAL SETUP ----------------
  */
 require('dotenv').config();
-
 var app = express();
 
 app.use(express.json());

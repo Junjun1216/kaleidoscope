ef0bed902ea6c015ebe0de2e92a4ac17621d55be
jun1216 <junchao.chen@mail.utoronto.ca>
2021-08-10 16:47:43 -0400

    update database to connect to mongo atlas

--- config/database.js
+++ config/database.js
@@ -10,14 +10,17 @@ require('dotenv').config();
  * Connect to MongoDB Server using the connection string in the `.env` file.  To implement this, place the following
  * string into the `.env` file
  *
- * DB_STRING=mongodb://<user>:<password>@localhost:27017/database_name
  */
 
 const conn = process.env.DB_STRING;
 
-const connection = mongoose.createConnection(conn, {
+var connection = mongoose.createConnection(conn, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+});
+
+connection.then((db) => {
+    console.log('We\'re Connected to Mongocluster!');
 });
 
 // Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
@@ -27,7 +30,6 @@ const UserSchema = new mongoose.Schema({
     salt: String,
     admin: Boolean
 });
-
 
 const User = connection.model('User', UserSchema);
 

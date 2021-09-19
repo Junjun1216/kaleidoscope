a83792bf689ce03c998c68a15e0b3bfdcae9c743
jun1216 <junchao.chen@mail.utoronto.ca>
2021-09-19 02:59:32 -0400

    typo in text

--- client/src/components/home/main.js
+++ client/src/components/home/main.js
@@ -12,15 +12,13 @@ const Main = () => {
         <div className="main">
             <div className="content">
                 <div className="intro_block">
-                    <Link to="/home">
-                        <Logo className="home_title" style_title={{ width: '400px', height: 'auto', margin: '10px 0 0 15px'}}
-                              style_logo={{display: "none", width: '75px', height: 'auto'}}/>
-                    </Link>
+                    <Logo className="home_title" style_title={{ width: '400px', height: 'auto', margin: '10px 0 0 15px'}}
+                          style_logo={{display: "none", width: '75px', height: 'auto'}}/>
                     <div className="home_description">
-                        A Simple Peer To Peer Video Conferencing Application Create From Express/React JS
+                        A Simple Peer To Peer Video Conferencing Application Created With Express/React JS
                     </div>
                     <Link to="/register">
-                        <input className="desc_btn" type="button" value="Get Started"/>
+                        <input className="desc_btn" type="button" value="Learn More"/>
                     </Link>
                 </div>
                 <div className="image_block">
@@ -31,7 +29,7 @@ const Main = () => {
                         <Link to="/register">
                             <input className="home_btn" type="button" value="Register"/>
                         </Link>
-                        <input className="home_btn about_btn" type="button" value="About"/>
+                        <input className="home_btn contact_btn" type="button" value="Contact"/>
                     </div>
                     <picture>
                         <source media="(min-width: 400px)" srcSet={conference}/>

8862a8f1226c115d74a7fd2713e5ea37a51a609e
Jun Chao Chen <junchao.chen@mail.utoronto.ca>
2021-09-19 20:44:48 -0400

    new footer wip

--- client/src/components/home/home_footer.js
+++ client/src/components/home/home_footer.js
@@ -5,35 +5,49 @@ import facebook from "../../resources/facebook.png";
 import linkedin from "../../resources/linkedln.png";
 import github from "../../resources/github.png";
 import about from "../../resources/about_icon.png";
+import contact from "../../resources/notebook-of-contacts.png";
+import source from "../../resources/open-source.png";
 
 const HomeFooter = () => {
 
     return (
         <div className="home_footer">
-            <Link to="/about">
+            <div className="img_wrap">
                 <picture>
                     <source media="(min-width: 400px)" srcSet={about}/>
-                    <img className="icon_links" src={about} alt="about_icon"/>
+                    <img className="icon_links about_link" src={about} alt="about_icon"/>
                 </picture>
-            </Link>
-            <a target="_blank" href="https://www.facebook.com/michaelchen1216/" rel="noreferrer">
+            </div>
+            <div className="img_wrap">
                 <picture>
-                    <source media="(min-width: 400px)" srcSet={facebook}/>
-                    <img className="icon_links" src={facebook} alt="facebook_icon"/>
+                    <source media="(min-width: 400px)" srcSet={contact}/>
+                    <img className="icon_links" src={contact} alt="contact_icon"/>
                 </picture>
-            </a>
-            <a target="_blank" href="https://github.com/Junjun1216" rel="noreferrer">
+            </div>
+            <div className="img_wrap">
                 <picture>
-                    <source media="(min-width: 400px)" srcSet={github}/>
-                    <img className="icon_links" src={github} alt="github_icon"/>
+                    <source media="(min-width: 400px)" srcSet={source}/>
+                    <img className="icon_links" src={source} alt="source_icon"/>
                 </picture>
-            </a>
-            <a target="_blank" href="https://www.linkedin.com/in/junchao-chen-17816b145/" rel="noreferrer">
-                <picture>
-                    <source media="(min-width: 400px)" srcSet={linkedin}/>
-                    <img className="icon_links" src={linkedin} alt="linkedin_icon"/>
-                </picture>
-            </a>
+            </div>
+            {/*<a target="_blank" href="https://www.facebook.com/michaelchen1216/" rel="noreferrer">*/}
+            {/*    <picture>*/}
+            {/*        <source media="(min-width: 400px)" srcSet={facebook}/>*/}
+            {/*        <img className="icon_links" src={facebook} alt="facebook_icon"/>*/}
+            {/*    </picture>*/}
+            {/*</a>*/}
+            {/*<a target="_blank" href="https://github.com/Junjun1216" rel="noreferrer">*/}
+            {/*    <picture>*/}
+            {/*        <source media="(min-width: 400px)" srcSet={github}/>*/}
+            {/*        <img className="icon_links" src={github} alt="github_icon"/>*/}
+            {/*    </picture>*/}
+            {/*</a>*/}
+            {/*<a target="_blank" href="https://www.linkedin.com/in/junchao-chen-17816b145/" rel="noreferrer">*/}
+            {/*    <picture>*/}
+            {/*        <source media="(min-width: 400px)" srcSet={linkedin}/>*/}
+            {/*        <img className="icon_links" src={linkedin} alt="linkedin_icon"/>*/}
+            {/*    </picture>*/}
+            {/*</a>*/}
         </div>
     )
 }
